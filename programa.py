d8f261c64ba0ac9367886bf41abf561866efb57e
gabrielUFCG <gabriel.coutinho@ccc.ufcg.edu.br>
2017-06-03 12:40:21 -0300

    adicionei getMinimo

--- programa.py
+++ programa.py
@@ -4,3 +4,10 @@ def getMaximo(listaInteiros):
 		if elemento > max:
 			max = elemento
 	return max
+
+def getMinimo(listaInteiros):
+	min = listaInteiros[0]
+	for elemento in listaInteiros:
+		if elemento < min:
+			min = elemento
+	return min
